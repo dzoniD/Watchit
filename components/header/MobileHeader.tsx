0713f671cb7d0e7f34cfa2840783c06a460f7637
dzoniD <edvard_dantes@hotmail.com>
2023-07-13 15:35:39 +0200

    mobile header

---
+++ components/header/MobileHeader.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { FiMenu, } from "react-icons/fi";
+import { CgSearch } from "react-icons/cg";
+import { FaUserLarge } from "react-icons/fa6";
+import styles from './header-mobile.module.css'
+import Link from 'next/link'
+
+export default function MobileHeader() {
+  return (
+    <header className={styles.header}>
+        <FiMenu color='white' size={35}/>
+        <div className={styles.logo}>
+          <p>🇼​​🇦​​🇹​​🇨​​🇭​​🇮​​🇹</p>
+        </div>
+        <div className={styles.icons__container}>
+        <CgSearch size={30} color='white'className={styles.searchIcon}/>
+        <FaUserLarge size={24}  color='white'/>
+        </div>
+    </header>
+  )
+}
